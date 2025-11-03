7a09ff6380fee7eeda415e69ac4e43af553e4b49
stefanstudiodirection <stefan.radovic@studiodirection.com>
2025-11-03 13:35:15 +0100

    review transfer

--- src/pages/ReviewTransfer.tsx
+++ src/pages/ReviewTransfer.tsx
@@ -92,8 +92,8 @@ export const ReviewTransfer: React.FC = () => {
             <div className="flex items-center justify-between">
               <div className="flex items-center gap-3">
                 <div 
-                  className="w-10 h-10 rounded-full flex items-center justify-center"
-                  style={{ backgroundColor: source.color }}
+                  className="w-10 h-10 flex items-center justify-center bg-[#211E1E] dark:bg-[#000] rounded-[4px]"
+                  style={{ color: source.color }}
                 >
                   <span className="text-2xl">{source.icon}</span>
                 </div>
